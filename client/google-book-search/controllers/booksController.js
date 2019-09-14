e29ee1c17922a4868ab9a973bf384ccb90de7d8e
Sherard <sherardg@pennmedicine.upenn.edu>
2019-09-14 14:38:47 -0400

    Updated controllers and book model

---
+++ client/google-book-search/controllers/booksController.js
@@ -0,0 +1,34 @@
+const db = require("../models");
+
+//Defining the methods used by the booksController
+module.exports = {
+    findAll: function(req, res) {
+        db.Book
+            .find(req.query)
+            .sort({ date: -1 })
+            .then(dbModel => res.json(dbMoel))
+            .catch(err => res.status(422).json(err));
+    },
+
+    findByID: function(req, res) {
+        db.Book
+            .find(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+
+    create: function(req, res) {
+        db.Book
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+
+    update: function(req, res) {
+        db.Book
+            .findByID({ _id: req.params.id })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+};
---
+++ client/google-book-search/models/book.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+
+    title: { type: String, required: true },
+    authors: { type: String, required: true},
+    description: { type: String },
+    image: String,
+    link: { type: String }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
