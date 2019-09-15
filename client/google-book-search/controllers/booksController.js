cce712904832044ea918d7eda3c3eefbce8e8077
Sherard <sherardg@pennmedicine.upenn.edu>
2019-09-15 18:08:23 -0400

    Updated controller

--- client/google-book-search/controllers/booksController.js
+++ client/google-book-search/controllers/booksController.js
@@ -6,7 +6,7 @@ module.exports = {
         db.Book
             .find(req.query)
             .sort({ date: -1 })
-            .then(dbModel => res.json(dbMoel))
+            .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
 
