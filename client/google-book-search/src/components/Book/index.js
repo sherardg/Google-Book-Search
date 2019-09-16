c536ecb18ef79c3e92a8bea8c9bf378abd7c9c77
Sherard <sherardg@pennmedicine.upenn.edu>
2019-09-16 12:38:43 -0400

    Updated Book Component

---
+++ client/google-book-search/src/components/Book/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { ListItem } from "../List";
+import { Row, Col } from "../Grid";
+
+function Book({ title, authors, description, link, image, Button}) {
+    return (
+        <ListItem>
+            <Row className="flex-wrap-reverse">
+                <Col size="md-8">
+                    <h3>{title}</h3>
+                </Col>
+                <Col size="md-4">
+                    <div className="btn-container">
+                        <a className="btn btn-light" target="_blank" rel="noopener noreferrer" href={link}>
+                            View
+                        </a>
+                        <Button />
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col size="md-6">
+                    <p>Writen by {authors}</p>
+                </Col>                
+            </Row>
+            <Row>
+                <Col size="12 sm-4 md-2">
+                    <img className="img-thumbnail img-fluid w-100" src={image} alt={title} />
+                </Col>
+                <Col size="12 sm-8 md-10">
+                    <p>{description}</p>
+                </Col>
+            </Row>
+        </ListItem>
+    );
+}
+
+export default Book;
