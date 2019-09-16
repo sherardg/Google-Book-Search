7ea81f386076010142fcb71fbbdf0f3581fc8ce5
Sherard <sherardg@pennmedicine.upenn.edu>
2019-09-15 21:57:42 -0400

    Updated utils

---
+++ client/google-book-search/src/utils/API.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export default {
+//Get books from the Google API
+getBooks: function(q) {
+    return axios.get("/api/google", { params: { q: "title:" + q } });
+},
+
+//Get saved books
+getSavedBooks: function () {
+    return axios.get("/api/books");
+},
+
+//Deletes the save book with the id
+deleteBook: function(id) {
+    return axios.delete("/api/books" + id);
+},
+
+//Saves a book to the database
+saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
+}
+
+};
