06f67918fd2eea60aed94841e0dce727564add85
Sherard <sherardg@pennmedicine.upenn.edu>
2019-09-23 16:45:17 -0400

    Updated server and client/src/App.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const PORT = process.env.PORT || 3001;
-const app = express();
 const mongoose = require("mongoose");
-const routes = require("express");
+const app = express();
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
